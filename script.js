d125bc37a9a211ed5908c50589399b2516aba32d
xxxnamed <3xnamed@gmail.com>
2025-04-08 02:13:34 +0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Переключение выдвижного меню по клику на гамбургер
-document.getElementById('hamburger').addEventListener('click', function() {
-  document.getElementById('sideMenu').classList.toggle('active');
-});
